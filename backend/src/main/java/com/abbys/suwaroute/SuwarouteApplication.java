22da459fcdfd0766bb83f593c5e1008971152b78
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-22 22:03:08 +0530

    setup the backend project

---
+++ backend/src/main/java/com/abbys/suwaroute/SuwarouteApplication.java
@@ -0,0 +1,13 @@
+package com.abbys.suwaroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuwarouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuwarouteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/abbys/suwaroute/SuwarouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbys.suwaroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuwarouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
