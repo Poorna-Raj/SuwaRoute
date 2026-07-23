1f4a86219897a6408a9aa1f1b615f54a8ad6ead5
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-23 14:46:53 +0530

    define the model class for the emergency

---
+++ backend/src/main/java/com/abbys/suwaroute/model/emergency/Emergency.java
@@ -0,0 +1,26 @@
+package com.abbys.suwaroute.model.emergency;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emergency {
+    private String emergencyId;
+    private String callerName;
+    private String callerNumber;
+    private double longitude;
+    private double latitude;
+    private String nearestNodeId;
+    private EmergencySeverity severity;
+    private EmergencyStatus status;
+    private String assignedAmbulanceId;
+    private String assignedHospitalId;
+    private LocalDateTime createdAt;
+}
