681fdf9ba81ea87345def39efe91365e7de9fa9f
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-23 12:41:35 +0530

    define the service class for the ambulance

---
+++ backend/src/main/java/com/abbys/suwaroute/service/AmbulanceService.java
@@ -0,0 +1,67 @@
+package com.abbys.suwaroute.service;
+
+import com.abbys.suwaroute.common.exception.DuplicateResourceException;
+import com.abbys.suwaroute.common.exception.ResourceNotFoundException;
+import com.abbys.suwaroute.model.ambulance.Ambulance;
+import com.abbys.suwaroute.model.ambulance.AmbulanceStatus;
+import com.abbys.suwaroute.repository.firebase.AmbulanceRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AmbulanceService {
+
+    private final AmbulanceRepository ambulanceRepository;
+
+    public AmbulanceService(AmbulanceRepository ambulanceRepository) {
+        this.ambulanceRepository = ambulanceRepository;
+    }
+
+    public Ambulance createAmbulance(Ambulance ambulance) {
+
+        if (ambulanceRepository.findById(ambulance.getAmbulanceId()) != null) {
+            throw new DuplicateResourceException("Ambulance already exists.");
+        }
+
+        if (ambulance.getStatus() == null) {
+            ambulance.setStatus(AmbulanceStatus.AVAILABLE);
+        }
+
+        return ambulanceRepository.save(ambulance);
+    }
+
+    public Ambulance getAmbulanceById(String id) {
+
+        Ambulance ambulance = ambulanceRepository.findById(id);
+
+        if (ambulance == null) {
+            throw new ResourceNotFoundException("Ambulance not found.");
+        }
+
+        return ambulance;
+    }
+
+    public List<Ambulance> getAllAmbulances() {
+        return ambulanceRepository.findAll();
+    }
+
+    public Ambulance updateAmbulance(Ambulance ambulance) {
+
+        if (ambulanceRepository.findById(ambulance.getAmbulanceId()) == null) {
+            throw new ResourceNotFoundException("Ambulance not found.");
+        }
+
+        return ambulanceRepository.update(ambulance);
+    }
+
+    public void deleteAmbulance(String id) {
+
+        if (ambulanceRepository.findById(id) == null) {
+            throw new ResourceNotFoundException("Ambulance not found.");
+        }
+
+        ambulanceRepository.delete(id);
+    }
+
+}
