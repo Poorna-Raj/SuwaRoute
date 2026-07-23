379ad18c6134b065bc3660d8feb5a96527c52cdb
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-23 10:29:09 +0530

    migrate to firestore

--- backend/src/main/java/com/abbys/suwaroute/config/FirebaseConfig.java
+++ backend/src/main/java/com/abbys/suwaroute/config/FirebaseConfig.java
@@ -3,33 +3,45 @@ package com.abbys.suwaroute.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
-    @Value("${firebase.database.url}")
-    private String databaseUrl;
 
     @PostConstruct
-    public void initialize(){
-        try{
+    public void initialize() {
+
+        try {
+
             InputStream serviceAccount = getClass()
                     .getClassLoader()
                     .getResourceAsStream("firebase/serviceAccountKey.json");
+
             assert serviceAccount != null;
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl(databaseUrl)
                     .build();
-            if(FirebaseApp.getApps().isEmpty()){
+
+            if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
             }
-        } catch (Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
+
+    @Bean
+    public Firestore firestore() {
+        return FirestoreClient.getFirestore();
+    }
+
 }
