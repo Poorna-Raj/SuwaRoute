7197b5eefdfeb64bdb915d89cfde6c9bfa245fe8
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-23 13:11:32 +0530

    define the model class for hospitals

---
+++ backend/src/main/java/com/abbys/suwaroute/model/hospital/Hospital.java
@@ -0,0 +1,20 @@
+package com.abbys.suwaroute.model.hospital;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hospital {
+    private String hospitalId;
+    private String hospitalName;
+    private double longitude;
+    private double latitude;
+    private String currentNode;
+    private int availableIcuBeds;
+    private String contactNumber;
+}
