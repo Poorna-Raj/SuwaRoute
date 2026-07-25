9f2577f99562a99bdc5e6b5986dcdc583897732a
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-25 13:36:45 +0530

    define the main class method for testing

---
+++ backend/src/main/java/com/abbys/suwaroute/TestDriver.java
@@ -0,0 +1,13 @@
+package com.abbys.suwaroute;
+
+import com.abbys.suwaroute.common.parser.GraphLoader;
+import com.abbys.suwaroute.model.graph.Graph;
+
+public class TestDriver {
+    public static void main(String[] args){
+        GraphLoader gl = new GraphLoader();
+        Graph graph = gl.load("src/main/resources/graph/graph.ser");
+
+        System.out.println(graph.getNodes().size());
+    }
+}
