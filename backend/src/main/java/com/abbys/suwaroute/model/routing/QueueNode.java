ec1b10e45ef7bc7974b82e7783586625b422e8be
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-25 15:54:33 +0530

    implement the comparable interface to add the priority queue

--- backend/src/main/java/com/abbys/suwaroute/model/routing/QueueNode.java
+++ backend/src/main/java/com/abbys/suwaroute/model/routing/QueueNode.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class QueueNode {
+public class QueueNode implements Comparable<QueueNode> {
     private long nodeId;
     private double distance;
+
+    @Override
+    public int compareTo(QueueNode o) {
+        return Double.compare(this.distance,o.distance);
+    }
 }
