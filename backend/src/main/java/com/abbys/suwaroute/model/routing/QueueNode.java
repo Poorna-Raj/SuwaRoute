02491f7db3a863ba9a7b5d46911f0b91f0c38011
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-25 13:21:28 +0530

    define the model class for queue nodes

---
+++ backend/src/main/java/com/abbys/suwaroute/model/routing/QueueNode.java
@@ -0,0 +1,11 @@
+package com.abbys.suwaroute.model.routing;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class QueueNode {
+    private long nodeId;
+    private double distance;
+}
