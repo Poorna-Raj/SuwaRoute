2156bfbb7b9cf9bca6c2f1cb258c686e62fdc8b5
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-26 12:23:24 +0530

    implement the nearest node service to find the nearest node based on the given longitude and latitude

---
+++ backend/src/main/java/com/abbys/suwaroute/service/NearestNodeService.java
@@ -0,0 +1,35 @@
+package com.abbys.suwaroute.service;
+
+import com.abbys.suwaroute.common.utils.GeoUtils;
+import com.abbys.suwaroute.model.graph.Graph;
+import com.abbys.suwaroute.model.osm.OsmNode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class NearestNodeService {
+    private final Graph graph;
+
+    public long findNearestNode(double latitude, double longitude){
+        double shortestDistance = Double.MAX_VALUE;
+        long nearestNodeId = -1;
+
+        for(OsmNode node: graph.getNodes().values()){
+            double distance = GeoUtils.haversine(
+                    latitude,
+                    longitude,
+                    node.getLatitude(),
+                    node.getLongitude()
+            );
+
+            if(distance < shortestDistance){
+                shortestDistance = distance;
+                nearestNodeId = node.getId();
+            }
+        }
+
+        return nearestNodeId;
+    }
+
+}
