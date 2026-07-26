30074a792520d351f5c3fed42caf56a3eb7acccd
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-26 14:29:28 +0530

    implement the controller class for routes to expose the service methods

---
+++ backend/src/main/java/com/abbys/suwaroute/controller/RouteController.java
@@ -0,0 +1,37 @@
+package com.abbys.suwaroute.controller;
+
+import com.abbys.suwaroute.common.dto.RouteRequest;
+import com.abbys.suwaroute.common.dto.RouteResponse;
+import com.abbys.suwaroute.service.RoutingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/routes")
+@AllArgsConstructor
+public class RouteController {
+    private final RoutingService routingService;
+
+    @PostMapping
+    public ResponseEntity<RouteResponse> calculateRoute(
+            @RequestBody RouteRequest request
+    ) {
+
+        RouteResponse response = routingService.calculateRoute(
+                request.getStartLatitude(),
+                request.getStartLongitude(),
+                request.getEndLatitude(),
+                request.getEndLongitude()
+        );
+
+        if (response == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(response);
+    }
+}
