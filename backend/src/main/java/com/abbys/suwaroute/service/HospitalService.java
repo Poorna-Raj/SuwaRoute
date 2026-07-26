c3f31a2a543e1a4afdcda2073e389e937a3655ce
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-26 15:59:25 +0530

    define the service method to return the available hospitals

--- backend/src/main/java/com/abbys/suwaroute/service/HospitalService.java
+++ backend/src/main/java/com/abbys/suwaroute/service/HospitalService.java
@@ -68,4 +68,8 @@ public class HospitalService {
 
         repository.delete(id);
     }
+
+    public List<Hospital> findHospitalsWithAvailableBeds() {
+        return repository.findHospitalsWithAvailableBeds();
+    }
 }
