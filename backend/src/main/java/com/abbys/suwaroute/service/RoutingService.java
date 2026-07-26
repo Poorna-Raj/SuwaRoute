bde4abe10a4bbea750a0190b81a26d4695ee01c8
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-07-26 14:28:13 +0530

    update the route service to return proper response

--- backend/src/main/java/com/abbys/suwaroute/service/RoutingService.java
+++ backend/src/main/java/com/abbys/suwaroute/service/RoutingService.java
@@ -1,28 +1,55 @@
 package com.abbys.suwaroute.service;
 
+import com.abbys.suwaroute.common.dto.Coordinate;
+import com.abbys.suwaroute.common.dto.RouteResponse;
+import com.abbys.suwaroute.model.graph.Graph;
+import com.abbys.suwaroute.model.osm.OsmNode;
 import com.abbys.suwaroute.model.routing.RouteResult;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class RoutingService {
+
     private final NearestNodeService nodeService;
     private final DijkstraService dijkstraService;
+    private final Graph graph;
 
-    public RouteResult calculateRoute(
+    public RouteResponse calculateRoute(
             double startLat,
             double startLon,
             double endLat,
             double endLon
-    ){
-        long source = nodeService.findNearestNode(startLat,startLon);
-        long destination = nodeService.findNearestNode(endLat,endLon);
+    ) {
 
-        return dijkstraService.shortestPath(
-                source,
-                destination
-        );
+        long source = nodeService.findNearestNode(startLat, startLon);
+        long destination = nodeService.findNearestNode(endLat, endLon);
+
+        RouteResult routeResult = dijkstraService.shortestPath(source, destination);
+
+        if (routeResult == null) {
+            return null;
+        }
+
+        List<Coordinate> path = new ArrayList<>();
+
+        for (Long nodeId : routeResult.getNodeIds()) {
+
+            OsmNode node = graph.getNodes().get(nodeId);
+
+            path.add(new Coordinate(
+                    node.getLatitude(),
+                    node.getLongitude()
+            ));
+        }
+
+        return RouteResponse.builder()
+                .distanceMeters(routeResult.getTotalDistance())
+                .path(path)
+                .build();
     }
 }
