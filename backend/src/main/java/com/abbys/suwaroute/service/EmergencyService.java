81f4cd5b89dcfe27d6fc3b7479b39ee29c4632d2
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-08-02 10:14:31 +0530

    update the emergency service to remove duplicate service which doesn't have emergency id

--- backend/src/main/java/com/abbys/suwaroute/service/EmergencyService.java
+++ backend/src/main/java/com/abbys/suwaroute/service/EmergencyService.java
@@ -1,6 +1,5 @@
 package com.abbys.suwaroute.service;
 
-import com.abbys.suwaroute.common.exception.DuplicateResourceException;
 import com.abbys.suwaroute.common.exception.ResourceNotFoundException;
 import com.abbys.suwaroute.model.ambulance.Ambulance;
 import com.abbys.suwaroute.model.ambulance.AmbulanceStatus;
@@ -34,12 +33,6 @@ public class EmergencyService {
     }
 
     public Emergency createEmergency(Emergency emergency) {
-
-        if (repository.findById(emergency.getEmergencyId()) != null) {
-            throw new DuplicateResourceException(
-                    "Emergency with ID '" + emergency.getEmergencyId() + "' already exists."
-            );
-        }
 
         if (emergency.getStatus() == null) {
             emergency.setStatus(EmergencyStatus.PENDING);
